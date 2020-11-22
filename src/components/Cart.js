295e8ca79c5060a6836618ec449d7c2b8e4c976d
Jamie Cook <jamie.p.cook@outlook.com>
2020-11-22 18:00:45 +0100

    code along completed

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,25 +1,31 @@
-import React from 'react'
-import { CartItem } from './CartItem'
+import React from "react";
+import { useSelector } from "react-redux";
+
+import { CartItem } from "./CartItem";
 
 export const Cart = () => {
-  // TODO - fetch products from the cart store
-  const products = []
-
-  // TODO - calculate total from the sum of all products in the cart
-  const totalPrice = 0
+  const products = useSelector(store => store.cart.items);
+  const totalPrice = useSelector(store =>
+    store.cart.items.reduce(
+      (total, item) => total + item.price * item.quantity,
+      0
+    )
+  );
 
   return (
     <div className="cart">
       <div className="total">
-        <span className="emoji" role="img" aria-label="cart">🛒</span>
+        <span className="emoji" role="img" aria-label="cart">
+          🛒
+        </span>
         <div className="amount">Total: {totalPrice}:-</div>
       </div>
 
       <ul className="items">
-        {products.map((product) => (
+        {products.map(product => (
           <CartItem key={product.id} product={product} />
         ))}
       </ul>
     </div>
-  )
-}
+  );
+};
